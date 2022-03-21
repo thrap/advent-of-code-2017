64e0b0eb776a413b06cf207196925b1b1dd44ced
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-21 23:47:51 +0100

    Solve day 11 part 2

--- README.md
+++ README.md
@@ -21,7 +21,7 @@
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
-[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%86/yellow)](src/day11)
+[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
@@ -139,9 +139,9 @@ Both parts: 0.579062ms
 
 ```
 Day 11
-Time part 1: 2.718ms
-Time part 2: -
-Both parts: 2.71775ms
+Time part 1: 2.494ms
+Time part 2: 2.674ms
+Both parts: 5.16826ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 21/50
-Total time: 490.031ms
+Total stars: 22/50
+Total time: 492.482ms
 ```
 
 <!--/RESULTS-->
--- src/day11/index.js
+++ src/day11/index.js
@@ -1,9 +1,5 @@
 import run from "aocrunner"
 
-const re = /(.*)/
-const parseLine = l => l.match(re).slice(1).map(x => +x ? +x : x)
-const parseInput = rawInput => rawInput.split(',')
-
 const dirs = {
   'n':  [0,  2],
   'se': [1, -1],
@@ -13,27 +9,23 @@ const dirs = {
   'ne': [1,  1]
 }
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
+const steps = rawInput => rawInput.split(',').map(x => dirs[x])
 
+const solve = (steps, f) => {
   var x = 0
   var y = 0
-  input.map(x => dirs[x]).forEach(([dx, dy]) => {
+  return steps.reduce((acc, [dx, dy]) => {
     x += dx
     y += dy
-  })
-  return (Math.abs(x) + Math.abs(y))/2
-}
-
-const part2 = (rawInput) => {
-
+    return f((Math.abs(x) + Math.abs(y))/2, acc)
+  }, 0)
 }
 
 run({
   part1: {
-    solution: part1,
+    solution: input => solve(steps(input), x => x)
   },
   part2: {
-    solution: part2,
+    solution: input => solve(steps(input), Math.max)
   },
 })
