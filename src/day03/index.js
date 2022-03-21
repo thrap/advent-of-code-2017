5241d7502974024b1987f461547aa13e5036ffca
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-21 16:32:35 +0100

    Solve day 3 part 2

--- src/day03/index.js
+++ src/day03/index.js
@@ -1,7 +1,5 @@
 import run from "aocrunner"
 
-const parseInput = rawInput => +rawInput
-
 const part1 = (input) => {
   var min = Number.MAX_VALUE
   for (var layer = 0; 4*(layer-1)*(layer-1) <= input; layer ++) {
@@ -14,10 +12,36 @@ const part1 = (input) => {
   return min
 }
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+const part2 = (input) => {
+  const dirs = [[0, 1], [-1, 0], [0, -1], [1, 0]]
 
-  return
+  const values = {'0,0': 1}
+
+  const around = ([x, y]) => {
+    const dirs = [[0,1],[1,0],[-1,0],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]
+    return dirs.map(([dx, dy]) => values[[x+dx, y+dy]]).filter(x => x).reduce((acc, x) => acc + x)
+  }
+
+  var pos = [0, 0]
+  for (var layer = 1; true; layer ++) {
+    pos[0]+=1
+    values[pos] = around(pos)
+    var start = (2*layer-1)*(2*layer-1)+1
+    var end = (2*layer+1)*(2*layer+1)
+
+    for (var dirI = 0; dirI <= 3; dirI++) {
+      var numSteps = (end-start+1)/4 - (dirI == 0 ? 1 : 0)
+      for (var step = 0; step < numSteps; step++) {
+        var [dx, dy] = dirs[dirI]
+        pos[0] += dx
+        pos[1] += dy
+        values[pos] = around(pos)
+        if (values[pos] > input) {
+          return values[pos]
+        }
+      }
+    }
+  }
 }
 
 run({
@@ -25,9 +49,6 @@ run({
     solution: part1,
   },
   part2: {
-    tests: [
-      //{ input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
 })
