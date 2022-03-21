da5b608cd4c9638a2843677b5aa4ab369f3e7509
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-21 13:28:47 +0100

    Solve day 1 part 2

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.248ms
-Time part 2: -
-Both parts: 0.247742ms
+Time part 1: 0.304ms
+Time part 2: 0.145ms
+Both parts: 0.449842ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 0.248ms
+Total stars: 2/50
+Total time: 0.45ms
 ```
 
 <!--/RESULTS-->
--- src/day01/index.js
+++ src/day01/index.js
@@ -1,34 +1,19 @@
 import run from "aocrunner"
 
-const part1 = (input) => {
+const solve = (input, next) => {
   var sum = 0
   for (var i = 0; i < input.length; i++) {
-    if (input[i] == input[(i+1)%input.length])
+    if (input[i] == input[next(i)%input.length])
       sum += +input[i]
   }
   return sum
 }
 
-const part2 = (rawInput) => {
-  //const input = parseInput(rawInput)
-
-  return
-}
-
-const part1Input = ``
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: '' },
-    ],
-    solution: part1,
+    solution: (input) => solve(input, x => x+1),
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
-    solution: part2,
+    solution: (input) => solve(input, x => x+input.length/2),
   },
-  onlyTests: false,
 })
