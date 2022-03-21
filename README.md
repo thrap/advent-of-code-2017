de1c557fd9192d564e5d4ed2b2050124e16a481f
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-21 17:08:31 +0100

    Solve day 4 part 2

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: 0.30949ms
 
 ```
 Day 03
-Time part 1: 0.2ms
-Time part 2: 1.054ms
-Both parts: 1.254235ms
+Time part 1: 0.167ms
+Time part 2: 0.938ms
+Both parts: 1.105057ms
 ```
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 4.991ms
+Time part 2: 13.376ms
+Both parts: 18.366577ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 1.977ms
+Total stars: 8/50
+Total time: 20.194ms
 ```
 
 <!--/RESULTS-->
--- src/day04/index.js
+++ src/day04/index.js
@@ -1,38 +1,24 @@
 import run from "aocrunner"
 
-const parseInput = rawInput => rawInput.split('\n').map(l => l.split(' '))
+const parse = input => input.split('\n').map(l => l.split(' '))
 
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
-  return input.filter(pass => {
+const solve = (input, f) => {
+  return parse(input).filter(pass => {
     var seen = {}
     for (var i = 0; i < pass.length; i++) {
-      if (seen[pass[i]])
+      if (seen[f(pass[i])])
         return false
-      seen[pass[i]] = true
+      seen[f(pass[i])] = true
     }
     return true
   }).length
 }
 
-const part2 = (rawInput) => {
-
-}
-
-const part1Input = ``
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: '' },
-    ],
-    solution: part1,
+    solution: input => solve(input, word => word),
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
-    solution: part2,
+    solution: input => solve(input, ([...word]) => word.sort()),
   },
-  onlyTests: false,
 })
