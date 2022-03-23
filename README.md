119888204f73aa96f49bbd42c251ddd0dacf4d6c
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-23 13:03:17 +0100

    Solve day 22 part 2

--- README.md
+++ README.md
@@ -32,7 +32,7 @@
 [![Day](https://badgen.net/badge/19/%E2%98%85%E2%98%85/green)](src/day19)
 [![Day](https://badgen.net/badge/20/%E2%98%85%E2%98%85/green)](src/day20)
 [![Day](https://badgen.net/badge/21/%E2%98%85%E2%98%85/green)](src/day21)
-[![Day](https://badgen.net/badge/22/%E2%98%85%E2%98%86/yellow)](src/day22)
+[![Day](https://badgen.net/badge/22/%E2%98%85%E2%98%85/green)](src/day22)
 ![Day](https://badgen.net/badge/23/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/24/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/25/%E2%98%86%E2%98%86/gray)
@@ -216,9 +216,9 @@ Both parts: 31.454576ms
 
 ```
 Day 22
-Time part 1: 13.032ms
-Time part 2: -
-Both parts: 13.032095ms
+Time part 1: 15.629ms
+Time part 2: 12426.294ms
+Both parts: 12441.923104ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 43/50
-Total time: 3324.035ms
+Total stars: 44/50
+Total time: 15752.926ms
 ```
 
 <!--/RESULTS-->
--- src/day22/index.js
+++ src/day22/index.js
@@ -1,18 +1,20 @@
 import run from "aocrunner"
 
-const parseInput = rawInput => rawInput.split('\n')
+const parseInput = rawInput => {
+  const input = rawInput.split('\n')
+  const grid = {}
+  input.forEach((l, i) => {
+    l.split('').forEach((c, j) => {
+      grid[[j-(input.length-1)/2, i-(input.length-1)/2]] = c == '#' ? 2 : 0
+    })
+  })
+  return grid
+}
 
 const dirs = [[0,-1],[-1,0],[0,1],[1,0]]
 
 const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
-
-  const grid = {}
-  input.forEach((l, i) => {
-    l.split('').forEach((c, j) => {
-      grid[[j-(input.length-1)/2, i-(input.length-1)/2]] = c == '#'
-    })
-  })
+  const grid = parseInput(rawInput)
 
   var x = 0
   var y = 0
@@ -37,27 +39,41 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const grid = parseInput(rawInput)
 
-  return
+  var x = 0
+  var y = 0
+  var dir = 0
+
+  var count = 0
+  for(var steps = 0; steps < 10000000; steps++) {
+    if (steps % 1000000 == 0) {
+      console.log(steps / 1000000 + '/10', count);
+    }
+    const c = grid[[x, y]] || 0
+
+    if (c == 2) {
+      dir = (4+dir-1)%4
+    } else if (c == 1) {
+      count++
+    } else if (c == 3) {
+      dir = (dir+2)%4
+    } else {
+      dir = (dir+1)%4
+    }
+
+    grid[[x, y]] = ((grid[[x, y]] || 0) + 1)%4
+    x += dirs[dir][0]
+    y += dirs[dir][1]
+  }
+  return count
 }
 
-const part1Input = `..#
-#..
-...`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: '' },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
