c84c36ac0e04f3c1dfe3f29223af1f65519d2cef
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-23 10:22:54 +0100

    Solve day 21 part 2 (SLOW ~6 sec)

--- README.md
+++ README.md
@@ -31,7 +31,7 @@
 [![Day](https://badgen.net/badge/18/%E2%98%85%E2%98%85/green)](src/day18)
 [![Day](https://badgen.net/badge/19/%E2%98%85%E2%98%85/green)](src/day19)
 [![Day](https://badgen.net/badge/20/%E2%98%85%E2%98%85/green)](src/day20)
-[![Day](https://badgen.net/badge/21/%E2%98%85%E2%98%86/yellow)](src/day21)
+[![Day](https://badgen.net/badge/21/%E2%98%85%E2%98%85/green)](src/day21)
 ![Day](https://badgen.net/badge/22/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/23/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/24/%E2%98%86%E2%98%86/gray)
@@ -209,9 +209,9 @@ Both parts: 348.309268ms
 
 ```
 Day 21
-Time part 1: 6.029ms
-Time part 2: -
-Both parts: 6.028774ms
+Time part 1: 6.155ms
+Time part 2: 6890.937ms
+Both parts: 6897.092025ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 41/50
-Total time: 3285.577ms
+Total stars: 42/50
+Total time: 10176.64ms
 ```
 
 <!--/RESULTS-->
--- src/day21/index.js
+++ src/day21/index.js
@@ -97,7 +97,77 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
+  const input = parseInput(rawInput)
+  console.log(input)
+  Object.keys(input).forEach(pattern => {
+    /*onsole.log(pattern);
+    console.log(" -> ");
+    console.log(input[pattern]);
+    console.log();*/
+  })
 
+  const partition = (grid) => {
+    var n = grid.length % 2 == 0 ? 2 : 3
+    const parts = []
+    for (var di = 0; di < grid.length; di += n) {
+      const row = []
+      for (var dj = 0; dj < grid.length; dj+= n) {
+        const part = Array(n).fill(0).map(_ => Array(n))
+        for (var i = 0; i < n; i ++) {
+          for (var j = 0; j < n; j++) {
+            part[i][j] = grid[di+i][dj+j]
+          }
+        }
+        row.push(part)
+      }
+      parts.push(row)
+    }
+    return parts
+  }
+
+  const step = grid => {
+    const parts = partition(grid).map(row => row.map(part => {
+      for (var i = 0; i < 4; i++) {
+        const found = input[toStr(part)] || input[toStr(mirror(part))]
+        if (found) {
+          return toArr(found)
+        }
+        part = rotate(part)
+      }
+
+      for (var i = 0; i < 4; i++) {
+        console.log(toStr(part));
+        console.log();
+        console.log(toStr(mirror(part)));
+        console.log();
+        part = rotate(part)
+      }
+      throw "faen"
+    }))
+
+    var newArr = []
+
+    for (var i = 0; i < parts.length; i++) {
+      for (var j = 0; j < parts[i][0].length; j++) {
+        newArr.push(parts[i].map(m => m[j]).flat())
+      }
+    }
+
+    return newArr
+
+  }
+
+  const toArr = str => str.split('\n').map(l => l.split(''))
+  const toStr = arr => arr.map(l => l.join('')).join('\n')
+  var grid = toArr('.#.\n..#\n###')
+
+  for (var i = 0; i < 18; i++) {
+    grid = step(grid)
+    console.log(toStr(grid));
+    console.log();
+  }
+
+  return grid.map(l => l.join('')).join('').replace(/\./g,'').length
 }
 
 run({
